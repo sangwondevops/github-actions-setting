cd38273fef1f7dc7f708f6a3375c69fe88086aa2
Jmtgri <121868537+sksrhd14@users.noreply.github.com>
2025-02-26 00:12:45 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction version5
+          Learn GithubAction version6
         </a>
       </header>
     </div>
