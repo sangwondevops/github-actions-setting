eb343dcec2911514862ae4949b601f9ac91f37d1
r2d2c2 <64571095+r2d2c2@users.noreply.github.com>
2025-03-20 12:48:35 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction test2
+          Learn GithubAction mytest
         </a>
       </header>
     </div>
