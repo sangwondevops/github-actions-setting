d5edda2a1c1b9483d5dfc63ce5beefcce1e2b9f5
EconomyLim <bmw95885835@gmail.com>
2026-01-04 14:47:31 +0900

    update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd-1 aws1
+          Learn GithubAction cicd-1 aws2
         </a>
       </header>
     </div>
